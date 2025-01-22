fc4c7731514bc0e2af5a3636bc53f2af28388471
Jose <mortox1990@gmail.com>
2025-01-22 20:26:16 +0100

    Preogresando con las clases

--- proyectoHibernate_JoseIgnacio/src/main/java/org/example/entities/Animales.java
+++ proyectoHibernate_JoseIgnacio/src/main/java/org/example/entities/Animales.java
@@ -2,6 +2,54 @@ package org.example.entities;
 
 public class Animales {
 
+    /**
+     * Tenemos un refugio de animales perdidos de todo tipo: Perros, gatos, pájaritos, cerdos vietnamitas, serpientes y camaleones y... ¡hasta arañas!
+     * Cada animal tiene un nombre, especie, edad y una descripción de cómo se perdió
+     *
+     */
 
+    //atributos
 
+    private String NombreAnimal;
+    private enum Especie{
+        Perro, Gato, Pajaro, Cerdo, Serpiente, Lagarto, Tortuga, Anfibio, Aracnido
+    }
+    private int Edad;
+    private String descripcion;
+
+    //constructor vacio
+    public Animales(){}
+
+    //constructor con los atributos
+
+    public Animales(String nombreAnimal, int edad, String descripcion) {
+        this.NombreAnimal = nombreAnimal;
+        this.Edad = edad;
+        this.descripcion = descripcion;
+    }
+
+    //getters y setters
+    public String getNombreAnimal() {
+        return NombreAnimal;
+    }
+
+    public void setNombreAnimal(String nombreAnimal) {
+        NombreAnimal = nombreAnimal;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- proyectoHibernate_JoseIgnacio/src/main/java/org/example/entities/Familia.java
+++ proyectoHibernate_JoseIgnacio/src/main/java/org/example/entities/Familia.java
@@ -2,6 +2,51 @@ package org.example.entities;
 
 public class Familia {
 
+    /*
+    * datos de la familia que acoge un animal (nombre, edad, ciudad).
+    * */
+
+    //atributos
+    private String Nombre;
+    private int Edad;
+    private String Ciudad;
+
+    //constrcutor vacio
+    public Familia(){}
+
+    //constructor con atributos
 
 
+    public Familia(String nombre, int edad, String ciudad) {
+        this.Nombre = nombre;
+        this.Edad = edad;
+        this.Ciudad = ciudad;
+    }
+
+    //getters y setters
+
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
+
+    public String getCiudad() {
+        return Ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        Ciudad = ciudad;
+    }
 }
